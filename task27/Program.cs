c27cbcb417425b84a530ce701c963353d7bb2692
Olga <o.nikolaichenko@yandex.ru>
2022-12-12 20:20:05 +0300

    Added task 27 and 29

---
+++ task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число ");
+int n = int.Parse(Console.ReadLine());
+Print(GetSumOfNumbers(n));
+
+int GetSumOfNumbers (int num)
+{
+    int sum = num%10;
+    num = num/10; 
+    while (num > 0)
+    {
+        sum = sum + (num%10);
+        num = num/10;
+    } 
+    return sum;
+}
+
+void Print (int number)
+{
+    Console.Write(number);
+}
+
---
+++ task29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int n = 8;
+int[] arr = new int[n];
+Random rand = new Random();
+
+int [] GetRandomArray (int[] arr)
+{
+    for (int i = 0; i<n; i++) 
+    {
+        arr[i] = rand.Next(0,34);
+        Console.Write(arr[i] + " ");
+    }
+    return arr;
+}
+
+GetRandomArray(arr);
