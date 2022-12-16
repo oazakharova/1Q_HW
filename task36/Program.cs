3e2b40cdbe59e3bab6555876324d2b3ff50fb65a
Olga <o.nikolaichenko@yandex.ru>
2022-12-16 12:46:42 +0300

    Added task36 and task 38

---
+++ task36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size)
+{
+    int[] array1 = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array1[i] = new Random().Next(1, 10);
+    }
+    return array1;
+}
+
+void PrintArray(int[] array2)
+{
+    for (int i = 0; i < array2.Length; i++)
+    {
+        Console.Write($"{array2[i]} ");
+    }
+}
+
+int GetSumOfOddIndex(int[] array3)
+{
+    int sum = 0;
+    for (int i = 1; i < array3.Length; i = i + 2)
+    {
+        sum = sum + array3[i];
+    }
+    Console.WriteLine($"Сумма элементов на нечетных позициях = {sum}");
+    return sum;
+}
+
+int length = 4;
+int[] array = GetArray(length);
+PrintArray(array);
+Console.WriteLine();
+GetSumOfOddIndex(array);
---
+++ task38/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] GetArray(int size)
+{
+    double[] array1 = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array1[i] = new Random().Next(1, 100);
+    }
+    return array1;
+}
+
+void PrintArray(double[] array2)
+{
+    for (int i = 0; i < array2.Length; i++)
+    {
+        Console.Write($"{array2[i]} ");
+    }
+}
+
+double GetMaxMinDiff(double[] array3)
+{
+    double dif = 0;
+    double max = 0;
+    double min = array3[0];
+
+    foreach (double el in array3)
+    {
+        if (el < min)
+        {
+            min = el;
+        }
+        //else
+        //{
+        if (el > max)
+        {
+            max = el;
+        }
+        //}
+    }
+    dif = max - min;
+    Console.WriteLine($"Разница между максимальным и минимальным элементами = {dif}");
+    return dif;
+}
+
+int length = 6;
+double[] array = GetArray(length);
+PrintArray(array);
+Console.WriteLine();
+GetMaxMinDiff(array);
